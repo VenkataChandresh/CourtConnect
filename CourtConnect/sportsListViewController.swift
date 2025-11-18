86f1a9cd35b79f68c0c829a8b559e53ac59fdb53
VenkataChandresh <158313715+VenkataChandresh@users.noreply.github.com>
2025-11-18 12:46:29 -0600

    Initial clean Commit

---
+++ CourtConnect/sportsListViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class sportsListViewController: UIViewController {
+
+    @IBOutlet weak var userSearchedSport: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func searchSportBTN(_ sender: Any) {
+    }
+
+}
