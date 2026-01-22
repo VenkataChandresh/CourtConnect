6f99c06e36c4f7878c33bc050f3bbf1ea2b175a4
VenkataChandresh <158313715+VenkataChandresh@users.noreply.github.com>
2026-01-22 12:55:36 -0600

    initial commit

---
+++ backend/src/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const promisePool = pool.promise();
+
+pool.getConnection((err, connection) => {
+    if ( err) {
+        return console.error("Error connecting to the database:", err);
+    } else {
+        console.log("Database connection established successfully.");
+        connection.release();
+    }
+
+})
+
+module.exports = promisePool;
